ab8b8538db36ead9cedfbff1648e4b4f7263fc11
BillStephens2022 <two4onebill@yahoo.com>
2023-02-16 07:36:35 -0500

    removed routes and api folders - no longer needed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const {  authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
-const api = require('./routes/index.js');
 const db = require('./config/connection');
 
 const app = express();
@@ -17,7 +16,6 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', api);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
