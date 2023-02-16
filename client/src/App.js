f146b4485dca21956a26df6d38b8d1139f5af58c
jsedlak146 <jssedlak146@gmail.com>
2023-02-16 15:14:43 -0500

    testing hash router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloClient,
   ApolloProvider,
@@ -46,7 +46,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/transactions" element={<Transactions />} />
             <Route path="/transactions/add" element={<TransactionForm />} />
-            <Route path="/Analysis" element={<Analysis />} />
+            <Route path="/analysis" element={<Analysis />} />
             <Route
               path="*"
               element={<h1 className="display-2">Wrong page!</h1>}
