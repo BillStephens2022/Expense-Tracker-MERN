d87634aab66eb7abce0a96c555e1dde24278659a
jsedlak146 <jssedlak146@gmail.com>
2023-02-15 20:44:26 -0500

    seeding testimonials, all set up in js files, needs to appear in designated spot

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,15 +1,17 @@
 const db = require('../config/connection');
-const { User, Transaction } = require('../models');
+const { User, Transaction, Testimonials } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const transactionSeeds = require('./transactionSeeds.json');
+const testimonialSeeds = require('./testimonialSeeds.json');
 
 db.once('open', async () => {
   try {
     await Transaction.deleteMany({});
     await User.deleteMany({});
+    await Testimonials.deleteMany({});
 
     const dbUsers = await User.create(userSeeds);
-  
+    await Testimonials.create(testimonialSeeds);
 
     for (let i = 0; i < transactionSeeds.length; i++) {
       const { _id: transId } = await Transaction.create(transactionSeeds[i]);
