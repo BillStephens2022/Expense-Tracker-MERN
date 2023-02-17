2db7d052a2935c0c1144a0ad9432c7a9bf97dbbb
BillStephens2022 <two4onebill@yahoo.com>
2023-02-17 09:32:20 -0500

    styling changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -51,62 +51,62 @@ const Home = () => {
         <div className="col-md-6">
   <div className="about-div text-center">
     <h2 className="budget-title">Analyze your data</h2>
-    <img className="budget-image img-fluid shadow" src={chart} alt="expense-pic" style={{ maxWidth: "100%" }} />
+    <img className="budget-image img-fluid shadow mb-3" src={chart} alt="expense-pic" style={{ maxWidth: "100%" }} />
     <p className="budget-text">
-      With the Expense Tracker you can visualize your spendings in each category and whether or not it's essential or non-essential, based on your input.
+      Visualize your spending by category and whether or not it's essential or non-essential.
     </p>
   </div>
 </div>
 
         <div className="col-md-12 mt-5">
-          <h2 className="testimonials-title text-center">User Testimonials</h2>
+          <h2 className="testimonials-title text-center mb-4">User Testimonials</h2>
 
           <div className="row testimonial-row">
             <div className="col-md-2 mx-auto">
               <div className="testimonial text-center">
-                <p className="testimonial-text">
+                <p className="testimonial-text pr-2 pl-2 pt-2">
                   "I love using Expense Tracker! It's so easy to use and helps
                   me keep track of my spending."
                 </p>
-                <p className="testimonial-author">- Sarah J.</p>
+                <p className="testimonial-author pb-2">- Sarah J.</p>
               </div>
             </div>
             <div className="col-md-2 mx-auto">
               <div className="testimonial text-center">
-                <p className="testimonial-text">
+                <p className="testimonial-text pr-2 pl-2 pt-2">
                   "This app has been a game-changer for me. I've been able to
                   save more money and reach my financial goals faster."
                 </p>
-                <p className="testimonial-author">- Michael T.</p>
+                <p className="testimonial-author pb-2">- Michael T.</p>
               </div>
             </div>
             <div className="col-md-2 mx-auto">
               <div className="testimonial text-center">
-                <p className="testimonial-text">
+                <p className="testimonial-text pr-2 pl-2 pt-2">
                   "Expense Tracker has helped me identify areas where I can cut
                   back and save more money. It's a must-have for anyone looking
                   to get their finances in order."
                 </p>
-                <p className="testimonial-author">- John D.</p>
+                <p className="testimonial-author pb-2">- John D.</p>
               </div>
             </div>
             <div className="col-md-2 mx-auto">
               <div className="testimonial text-center">
-                <p className="testimonial-text">
+                <p className="testimonial-text pr-2 pl-2 pt-2">
                   "I've tried other budgeting apps, but Expense Tracker is by
                   far the best. It's intuitive, user-friendly, and has all the
                   features I need."
                 </p>
-                <p className="testimonial-author">- Mary R.</p>
+                <p className="testimonial-author pb-2">- Mary R.</p>
               </div>
             </div>
             <div className="col-md-2 mx-auto">
               <div className="testimonial text-center">
-                <p className="testimonial-text">
+                <p className="testimonial-text pr-2 pl-2 pt-2">
                   "Expense Tracker has helped me identify my spending patterns
                   and make smarter financial decisions. I highly recommend it!"
                 </p>
-                <p className="testimonial-author">- David L.</p>
+                <p className="testimonial-author pb-2">- David L.</p>
               </div>
             </div>
           </div>
