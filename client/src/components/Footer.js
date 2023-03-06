7589e1a4a366b4e17b60108156f63c2842d04a3e
BillStephens2022 <two4onebill@yahoo.com>
2023-03-06 16:50:12 -0500

    fixed some console errors

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,22 +3,22 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer text-center mt-5 py-3 bg-dark text-light">
-            <div class="row">
-              <div class="col-md-4">
+            <div className="row">
+              <div className="col-md-4">
                 <a href="https://www.facebook.com">
-                  <i class="fab fa-facebook fa-2x text-light mr-2"></i>
+                  <i className="fab fa-facebook fa-2x text-light mr-2"></i>
                 </a>
                 <a href="https://www.twitter.com">
-                  <i class="fab fa-twitter fa-2x text-light mr-2"></i>
+                  <i className="fab fa-twitter fa-2x text-light mr-2"></i>
                 </a>
                 <a href="https://www.instagram.com">
-                  <i class="fab fa-instagram fa-2x text-light"></i>
+                  <i className="fab fa-instagram fa-2x text-light"></i>
                 </a>
               </div>
-              <div class="col-md-4">
-                <p class="m-0">Contact us:</p>
-                <p class="m-0">contact@expensetracker.com</p>
-                <p class="m-0">(123) 456-7890</p>
+              <div className="col-md-4">
+                <p className="m-0">Contact us:</p>
+                <p className="m-0">contact@expensetracker.com</p>
+                <p className="m-0">(123) 456-7890</p>
               </div>
             </div>
             <div className="mt-3">
