e10e13e9945ded0bd9b2613379a3a23b3a795a1d
BillStephens2022 <two4onebill@yahoo.com>
2023-09-27 12:32:03 -0400

    removed some text from the Loginmodal

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -52,7 +52,7 @@ const LoginForm = () => {
           Something went wrong with your login credentials!
         </Alert>
         <Form.Group>
-          <Form.Label htmlFor='email'>Email (you can use 'bill@gmail.com' to test drive with pre-loaded data)</Form.Label>
+          <Form.Label htmlFor='email'>Email</Form.Label>
           <Form.Control
             type='text'
             placeholder='Your email'
@@ -65,7 +65,7 @@ const LoginForm = () => {
         </Form.Group>
 
         <Form.Group>
-          <Form.Label htmlFor='password'>Password (you can use 'password' to test drive with pre-loaded data)</Form.Label>
+          <Form.Label htmlFor='password'>Password</Form.Label>
           <Form.Control
             type='password'
             placeholder='Your password'
